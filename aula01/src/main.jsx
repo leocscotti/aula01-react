1d1f4ba7e49162f7dd5bd123278c269e75aa6998
leocscotti <leoscotti03@gmail.com>
2023-09-18 14:09:18 -0300

    Exemplos de useEffect x 3 - Tipos 2 e 3 com array de dependências.

--- aula01/src/main.jsx
+++ aula01/src/main.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
@@ -7,6 +6,7 @@ import Home from './routes/Home.jsx';
 import EditarProdutos from './routes/EditarProdutos.jsx';
 import Erro404 from './routes/Erro404.jsx';
 import Produtos from './routes/Produtos.jsx';
+import ExcluirProduto from './routes/ExcluirProduto.jsx';
 
 const router = createBrowserRouter([
   {path: '/', element: <App/>,
@@ -15,12 +15,13 @@ const router = createBrowserRouter([
       {path: '/', element: <Home/>},
       {path: '/produtos', element: <Produtos/>},
       {path: '/editar/produtos/:id', element: <EditarProdutos/>},
+      {path: '/excluir/produtos/:id', element: <ExcluirProduto/>},
     ]
   }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  
        <RouterProvider router={router}/>
-  </React.StrictMode>,
+  ,
 )
--- aula01/src/routes/Produtos.jsx
+++ aula01/src/routes/Produtos.jsx
@@ -1,38 +1,62 @@
 import { Link } from "react-router-dom";
 import { ListaProdutos } from "../components/ListaProdutos";
 import {GrFormEdit as Editar} from "react-icons/gr";
+import {RiDeleteBin2Fill as Excluir} from "react-icons/ri";
+import style from "./Produtos.module.css";
+import { useEffect, useState } from "react";
 
 export default function Produtos() {
   document.title = "Produtos";
 
-  const estiloDasCelulas = {
-    border:"2px solid #ccc",
-    fontFamily:"Arial, Trebuchet, sans-serif"
-  }
+  useEffect(()=>{
+    console.log("Este useEffect renderiza sempre! " + counter);
+  });
+  
+  const [counter, setCounter] = useState(0);
 
-  return (
+  const [listaProdutosLocal, setListaProdutosLocal] = useState([{}]);
+
+  useEffect(()=>{
+    console.log("Este useEffect renderiza apenas uma vez!");
+    setListaProdutosLocal(ListaProdutos);
+  },[ ]);
+
+  const [counter2, setCounter2] = useState(0)
+
+  useEffect(()=>{
+    console.log("Este useEffect renderiza sempre que o objeto/variável ou elemento que está sendo monitorado no array de dependências sofra alguma atualização.");
+  },[counter2]);
+
+
+
+  return ( 
     <div>
       <h1>LISTA DE PRODUTOS</h1>
-
-      <table style={{border:"2px solid #ccc", borderCollapse:"collapse", margin:"auto"}}>
+      <div>
+        <button onClick={()=> setCounter(counter + 1)}>COUNTER - {counter}</button>
+      </div>
+      <div>
+        <button onClick={()=> setCounter2(counter2 + 1)}>COUNTER2 - {counter2}</button>
+      </div>
+      <table className={style.tblEstilo}>
         <thead>
         <tr>
           <th>ID</th>
           <th>NOME</th>
           <th>DESCRIÇÃO</th>
           <th>PREÇO</th>
-          <th>EDITAR</th>       
+          <th>EDITAR / EXCLUIR</th>       
         </tr>
         </thead>
         <tbody>
         {
-          ListaProdutos.map((item,indice)=>(
-            <tr key={indice}>
-                <td style={estiloDasCelulas}>{item.id}</td>
-                <td style={estiloDasCelulas}>{item.nome}</td>
-                <td style={estiloDasCelulas}>{item.desc}</td>
-                <td style={estiloDasCelulas}>{item.valor}</td>
-                <td style={estiloDasCelulas}> <Link to={`/editar/produtos/${item.id}`}><Editar/></Link> </td>
+          listaProdutosLocal.map((item,indice)=>(
+            <tr key={indice} className={style.tblLine}>
+                <td>{item.id}</td>
+                <td>{item.nome}</td>
+                <td>{item.desc}</td>
+                <td>{item.valor}</td>
+                <td> <Link to={`/editar/produtos/${item.id}`}><Editar/></Link> | <Link to={`/excluir/produtos/${item.id}`}><Excluir/></Link> </td>
             </tr>
           ))
         }
