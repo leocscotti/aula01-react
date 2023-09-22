bcbd66782873356449a07d9cefb049c104cb870f
leocscotti <leoscotti03@gmail.com>
2023-09-22 09:44:48 -0300

    finalizando o inserir produto

--- aula01/src/routes/InserirProduto.jsx
+++ aula01/src/routes/InserirProduto.jsx
@@ -14,7 +14,49 @@ export default function InserirProduto() {
         valor:""
     });
 
+    const handleChange= (e)=>[
+        e.preventDefault();
+
+        const {name, value}= e.target;
+
+        setProduto({...produto,[name]:value});
+    ]
+
+    const handleSubmit= (e)=>[
+        e.preventDefault();
+        ListaProdutos.push(produto);
+        navigate("/produto");
+    ]
+
   return (
-    <div>Cadastro de Produtos</div>
+    <div>
+        <h1>Cadastro de Produtos</h1>
+        <div>
+        <form>
+            <fieldset>
+              <legend>PRODUTO</legend>
+              <div>
+                <label htmlFor="idNome">Nome do Produto:</label>
+                <input type="text" name="nome" id="idNome" value={produto.nome} onChange={handleChange}/>
+              </div>
+              <div>
+                <label htmlFor="idDesc">Descrição do Produto:</label>
+                <input type="text" name="desc" id="idDesc" value={produto.desc} onChange={handleChange}/>
+              </div>
+              <div>
+                <label htmlFor="idValor">Valor do Produto:</label>
+                <input type="text" name="valor" id="idValor" value={produto.valor} onChange={handleChange}/>
+              </div>
+              <div>
+                <button>EDITAR</button>
+              </div>
+            </fieldset>
+          </form>
+          </div>
+    </div>
+    
+        
+    
+
   )
 }
