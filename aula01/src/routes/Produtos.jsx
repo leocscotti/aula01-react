333aa83c7319aa51080376f7c780ca0089e6d04c
leocscotti <leoscotti03@gmail.com>
2023-09-29 08:41:13 -0300

    Estilizando imagens em componentes com CSS.MODULE.

--- aula01/src/routes/Produtos.jsx
+++ aula01/src/routes/Produtos.jsx
@@ -1,22 +1,32 @@
 import { Link } from "react-router-dom";
-import { ListaProdutos } from "../components/ListaProdutos";
 import {GrFormEdit as Editar} from "react-icons/gr";
 import {RiDeleteBin2Fill as Excluir} from "react-icons/ri";
 import style from "./Produtos.module.css";
+import { useEffect, useState } from "react";
 
 
 export default function Produtos() {
   document.title = "Produtos";
 
-  
+  const [listaProdutoExterna, setListaProdutoExterna] = useState([{}]);
 
+  useEffect(()=>{
+    fetch("http://localhost:5000/produtos",{
+      method: "GET",
+      headers:{
+        "Content-Type":"application/json"
+      }
+    })
+    .then((response)=> response.json())
+    .then((data) => {
+      setListaProdutoExterna(data);
+    })
+    .catch(error => console.log(error));
+  },[]);
 
   return ( 
     <div>
       <h1>LISTA DE PRODUTOS</h1>
-      <div>
-        
-      </div>
       <table className={style.tblEstilo}>
         <thead>
         <tr>
@@ -29,7 +39,7 @@ export default function Produtos() {
         </thead>
         <tbody>
         {
-          listaProdutosLocal.map((item,indice)=>(
+          listaProdutoExterna.map((item,indice)=>(
             <tr key={indice} className={style.tblLine}>
                 <td>{item.id}</td>
                 <td>{item.nome}</td>
@@ -49,26 +59,3 @@ export default function Produtos() {
     </div>
   )
 }
-
-// useEffect(()=>{
-//     console.log("Este useEffect renderiza sempre! " + counter);
-//   });
-  
-//   const [counter, setCounter] = useState(0);
-
-//   const [listaProdutosLocal, setListaProdutosLocal] = useState([{}]);
-
-//   useEffect(()=>{
-//     console.log("Este useEffect renderiza apenas uma vez!");
-//     setListaProdutosLocal(ListaProdutos);
-//   },[ ]);
-
-//   const [counter2, setCounter2] = useState(0)
-
-//   useEffect(()=>{
-//     console.log("Este useEffect renderiza sempre que o objeto/variável ou elemento que está sendo monitorado no array de dependências sofra alguma atualização.");
-//   },[counter2]);
-{/* <button onClick={()=> setCounter(counter + 1)}>COUNTER - {counter}</button>
-      </div>
-      <div>
-        <button onClick={()=> setCounter2(counter2 + 1)}>COUNTER2 - {counter2}</button> */}
