083425f010404d13103d3820f0880443dfde063c
leocscotti <leoscotti03@gmail.com>
2023-09-29 10:33:15 -0300

    inserindo o modal

--- aula01/src/routes/Produtos.jsx
+++ aula01/src/routes/Produtos.jsx
@@ -3,6 +3,7 @@ import {GrFormEdit as Editar} from "react-icons/gr";
 import {RiDeleteBin2Fill as Excluir} from "react-icons/ri";
 import style from "./Produtos.module.css";
 import { useEffect, useState } from "react";
+import ModalInserir from "../components/ModalInserir/ModalInserir";
 
 
 export default function Produtos() {
@@ -24,9 +25,16 @@ export default function Produtos() {
     .catch(error => console.log(error));
   },[]);
 
+  const [open, setOpen] = useState(false);
+
   return ( 
     <div>
       <h1>LISTA DE PRODUTOS</h1>
+
+      {open ? <ModalInserir open={open} setOpen={setOpen}/> : "" }
+
+      <button onClick={()=> setOpen(true)}>OPEN-MODAL</button>
+
       <table className={style.tblEstilo}>
         <thead>
         <tr>
@@ -59,3 +67,30 @@ export default function Produtos() {
     </div>
   )
 }
+
+//Exemplo de useEfect
+// useEffect(()=>{
+//   console.log("Este useEffect renderiza sempre! " + counter);
+// });
+
+// const [counter, setCounter] = useState(0);
+
+// const [listaProdutosLocal, setListaProdutosLocal] = useState([{}]);
+
+// useEffect(()=>{
+//   console.log("Este useEffect renderiza apenas uma vez!");
+//   setListaProdutosLocal(ListaProdutos);
+// },[ ]);
+
+// const [counter2, setCounter2] = useState(0)
+
+// useEffect(()=>{
+//   console.log("Este useEffect renderiza sempre que o objeto/variável ou elemento que está sendo monitorado no array de dependências sofra alguma atualização.");
+// },[counter2]);
+
+// <div>
+// <button onClick={()=> setCounter(counter + 1)}>COUNTER - {counter}</button>
+// </div>
+// <div>
+// <button onClick={()=> setCounter2(counter2 + 1)}>COUNTER2 - {counter2}</button>
+// </div>
